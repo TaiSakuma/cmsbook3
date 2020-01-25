f710868455483ce8de605d7be01de767115b3773
Tai Sakuma <tai.sakuma@gmail.com>
2020-01-25 14:23:25 -0500

    make the side navigation a component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,7 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer" app clipped>
-      <v-list>
-        <v-list-item link router :to="'/'">
-          <v-list-item-action>
-            <v-icon>mdi-book-open</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link router :to="'/about'">
-          <v-list-item-action>
-            <v-icon>mdi-information</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>About</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <SideNavi></SideNavi>
     </v-navigation-drawer>
     <v-app-bar app dense clipped-left>
       <v-app-bar-nav-icon @click="drawer = !drawer" />
@@ -41,12 +24,14 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld";
+import SideNavi from "./components/SideNavi";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    HelloWorld,
+    SideNavi
   },
 
   data: () => ({
---
+++ src/components/SideNavi.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-list>
+    <v-list-item link router :to="'/'">
+      <v-list-item-action>
+        <v-icon>mdi-book-open</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item link router :to="'/about'">
+      <v-list-item-action>
+        <v-icon>mdi-information</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>About</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: "SideNavi",
+  data: () => ({})
+};
+</script>
