cb3d12f313cc709f6373301cfef6fa4b66d7dffd
Tai Sakuma <tai.sakuma@gmail.com>
2020-01-25 13:56:57 -0500

    add navigation drawer and content

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list>
+        <v-list-item link router :to="'/'">
+          <v-list-item-action>
+            <v-icon>mdi-book-open</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link router :to="'/about'">
+          <v-list-item-action>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar app dense clipped-left>
       <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title>cmsbook</v-toolbar-title>
@@ -13,6 +33,9 @@
         <span>Toggle dark mode</span>
       </v-tooltip>
     </v-app-bar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
   </v-app>
 </template>
 
