6b526b85ce0084932c05d9418f3d05df3bf03c9e
Tai Sakuma <tai.sakuma@gmail.com>
2020-01-26 10:38:23 -0500

    implement redirection

--- src/views/Redirect.vue
+++ src/views/Redirect.vue
@@ -8,6 +8,11 @@
 
 <script>
 export default {
-  name: "redirect"
+  name: "redirect",
+  created() {
+    const pathTop = "http://localhost/~sakuma/cmsbook";
+    const path = pathTop + this.$route.path;
+    window.location = path;
+  }
 };
 </script>
