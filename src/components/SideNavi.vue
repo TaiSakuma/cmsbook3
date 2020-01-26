0af51203ead95e6467b0fa53e864b8a146a879ac
Tai Sakuma <tai.sakuma@gmail.com>
2020-01-25 20:20:56 -0500

    update links

--- src/components/SideNavi.vue
+++ src/components/SideNavi.vue
@@ -1,12 +1,12 @@
 <template>
   <v-card flat>
     <v-toolbar flat>
-          <v-toolbar-title v-text="$route.params.chapter"></v-toolbar-title>      
+      <v-toolbar-title v-text="$route.params.chapter"></v-toolbar-title>
     </v-toolbar>
     <v-list>
       <v-list-item link router v-for="page in pages" :key="page.path" :to="page.path">
         <v-list-item-action>
-          <v-icon >mdi-book-open</v-icon>
+          <v-icon>mdi-book-open</v-icon>
         </v-list-item-action>
         <v-list-item-content>
           <v-list-item-title v-text="page.name"></v-list-item-title>
@@ -22,26 +22,29 @@ import axios from "axios";
 export default {
   name: "SideNavi",
   data: () => ({
-    pages: [
-      { name: "Home", path: "/" },
-      { name: "About", path: "/about" },
-      { name: "Page", path: "/page" },
-      { name: "log", path: "/help/s0000_index_001/log" }
-    ],
-    scratch: null
+    pages: []
   }),
-  created() {
+  methods: {
+    updatePages() {
       const pathTop = "http://localhost/~sakuma/cmsbook";
       let path = pathTop + "/" + this.$route.params.chapter;
       path = path + "/cmsbook_frame/sections.json";
-      console.log(path);
       axios.get(path).then(response => {
-        let sections = response.data['sections'];
-        for(const s of sections) {
+        let sections = response.data["sections"];
+        for (const s of sections) {
           s["path"] = "/" + this.$route.params.chapter + "/" + s["path"];
         }
         this.pages = sections;
       });
+    }
+  },
+  created() {
+      this.updatePages();
+  },
+  watch: {
+    '$route.params.chapter': function() {
+        this.updatePages();
+    }
   }
 };
 </script>
