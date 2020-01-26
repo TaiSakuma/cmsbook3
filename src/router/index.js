426044f0d137e474b0cdb47cea98f0eaa34bd93e
Tai Sakuma <tai.sakuma@gmail.com>
2020-01-26 09:23:38 -0500

    add PageNotFound

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Page from "../views/Page.vue";
+import PageNotFound from "../views/PageNotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -23,6 +24,11 @@ const routes = [
     path: "/:chapter/:section/:page",
     name: "page",
     component: Page
+  },
+  {
+    path: "*",
+    name: "pagenotfound",
+    component: PageNotFound
   }
 ];
 
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pagenotfound">
+    <v-container>
+      <p>Page not found.</p>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pagenotfound"
+};
+</script>
