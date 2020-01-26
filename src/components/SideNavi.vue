3c3fc58d975d27841b2abc7148244adbbaf35b72
Tai Sakuma <tai.sakuma@gmail.com>
2020-01-25 19:44:49 -0500

    rename

--- src/components/SideNavi.vue
+++ src/components/SideNavi.vue
@@ -4,15 +4,16 @@
           <v-toolbar-title v-text="$route.params.chapter"></v-toolbar-title>      
     </v-toolbar>
     <v-list>
-      <v-list-item link router v-for="page in pages" :key="page.title" :to="page.to">
+      <v-list-item link router v-for="page in pages" :key="page.path" :to="page.path">
         <v-list-item-action>
           <v-icon v-text="page.icon"></v-icon>
         </v-list-item-action>
         <v-list-item-content>
-          <v-list-item-title v-text="page.title"></v-list-item-title>
+          <v-list-item-title v-text="page.name"></v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-list>
+    <p>{{ $route.path }}</p>
   </v-card>
 </template>
 
@@ -21,10 +22,10 @@ export default {
   name: "SideNavi",
   data: () => ({
     pages: [
-      { title: "Home", icon: "mdi-book-open", to: "/" },
-      { title: "About", icon: "mdi-information", to: "/about" },
-      { title: "Page", icon: "mdi-book-open", to: "/page" },
-      { title: "log", icon: "mdi-book-open", to: "/help/s0000_index_001/log" }
+      { name: "Home", icon: "mdi-book-open", path: "/" },
+      { name: "About", icon: "mdi-information", path: "/about" },
+      { name: "Page", icon: "mdi-book-open", path: "/page" },
+      { name: "log", icon: "mdi-book-open", path: "/help/s0000_index_001/log" }
     ]
   })
 };
