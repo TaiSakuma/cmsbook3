4a6d74bd07338769b11e611eaa5cabe0c776ac10
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-02 13:14:28 -0500

    support subcontents

--- src/components/SideNavi.vue
+++ src/components/SideNavi.vue
@@ -4,14 +4,43 @@
       <v-toolbar-title v-text="$route.params.chapter"></v-toolbar-title>
     </v-toolbar>
     <v-list>
-      <v-list-item link router v-for="page in pages" :key="page.path" :to="page.path">
-        <v-list-item-action>
-          <v-icon>mdi-book-open</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title v-text="page.name"></v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <template v-for="page in pages">
+        <template v-if="page.path">
+          <v-list-item
+            link
+            router
+            :key="page.name"
+            :to="'/' + $route.params.chapter + '/' + page.path"
+          >
+            <v-list-item-action>
+              <v-icon>mdi-book-open</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="page.name"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+        <template v-else-if="page.subcontents">
+          <v-list-group prepend-icon="mdi-book-open" value="true" :key="page.name">
+            <template v-slot:activator>
+              <v-list-item-title v-text="page.name"></v-list-item-title>
+            </template>
+            <v-list-item
+              link
+              router
+              v-for="subpage in page.subcontents"
+              :key="subpage.name"
+              :to="'/' + $route.params.chapter + '/' + subpage.path"
+            >
+            <v-list-item-action>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="subpage.name"></v-list-item-title>
+            </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </template>
+      </template>
     </v-list>
   </v-card>
 </template>
@@ -29,24 +58,22 @@ export default {
       const pathTop = "http://localhost/~sakuma/cmsbook";
       let path = pathTop + "/" + this.$route.params.chapter;
       path = path + "/.cmsbook3/sections.json";
-      axios.get(path).then(response => {
-        let sections = response.data["sections"];
-        for (const s of sections) {
-          s["path"] = "/" + this.$route.params.chapter + "/" + s["path"];
-        }
-        this.pages = sections;
-      })
-      .catch(error => {
-        this.pages = [ ]
-      });
+      axios
+        .get(path)
+        .then(response => {
+          this.pages = response.data.sections;
+        })
+        .catch(error => {
+          this.pages = [];
+        });
     }
   },
   created() {
-      this.updatePages();
+    this.updatePages();
   },
   watch: {
-    '$route.params.chapter': function() {
-        this.updatePages();
+    "$route.params.chapter": function() {
+      this.updatePages();
     }
   }
 };
