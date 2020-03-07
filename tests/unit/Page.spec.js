df256bfc061f3bd6d5e02be25cb32bf944ea43a9
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-07 12:39:43 -0500

    update tests for Page.vue

--- tests/unit/Page.spec.js
+++ tests/unit/Page.spec.js
@@ -3,36 +3,76 @@ import VueRouter from "vue-router";
 import Vuetify from "vuetify";
 import { mount, shallowMount, createLocalVue } from "@vue/test-utils";
 
+import moxios from "moxios";
+
+import $ from "jquery";
+
 import Page from "@/views/Page.vue";
 
 Vue.use(Vuetify);
 // Vue.use(VueRouter);
 
-const localVue = createLocalVue();
-// localVue.use(VueRouter);
-
 describe("Page.vue", () => {
-  let wrapper;
+  const ENV_ORG = process.env;
+  const $_ORG = global.$;
+  const MathJax_ORG = global.MathJax;
+
+  let localVue;
   let router;
+  let wrapper;
   beforeEach(() => {
+    process.env.VUE_APP_CMSBOOK_URL = "http://localhost/cmsbook";
+    global.$ = $;
+    global.MathJax = { typeset: jest.fn() }
+    moxios.install();
+    localVue = createLocalVue();
     router = new VueRouter();
-    wrapper = shallowMount(Page, {
+
+    wrapper = mount(Page, {
       localVue,
       router,
       mocks: {
         $route: {
+          path: "/chapter-A/section-b/page-3.md",
           params: {
-            chapter: "A",
-            section: "a",
-            page: "c.md"
+            chapter: "chapter-A",
+            section: "section-b",
+            page: "page-3.md"
           }
         }
       }
     });
   });
 
-  it("test text", () => {
-    console.log(wrapper.html());
-    // expect(wrapper.text()).toBe("This is an about page");
+  afterEach(() => {
+    moxios.uninstall();
+    global.MathJax = MathJax_ORG;
+    global.$ = $_ORG;
+    process.env = ENV_ORG;
+  });
+
+  it("server URL", () => {
+    expect(wrapper.vm.path).toBe(
+      "http://localhost/cmsbook/chapter-A/section-b/page-3.md"
+    );
+  });
+
+  it("test text", done => {
+    console.log(wrapper.vm.path);
+    moxios.wait(async () => {
+      let request = moxios.requests.mostRecent();
+      expect(request.config.url).toBe("http://localhost/cmsbook/chapter-A/section-b/page-3.md");
+      expect(request.config.method).toBe("get");
+      expect(request.config.data).toBeUndefined();
+
+      await request.respondWith({
+        status: 200,
+        response: "aaa"
+      })
+
+      console.log(request.config);
+      console.log(wrapper.html());
+      done();
+    });
   });
 });
