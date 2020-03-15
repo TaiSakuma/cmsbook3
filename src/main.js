45526002d6c04b81a9703efa9af2d2cb80761c04
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-15 14:09:47 -0400

    use Vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
+import Vuex from 'vuex'
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuex);
 
 new Vue({
   router,
