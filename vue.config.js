cb0a9da838426821724c5284441746b58d5b78b0
Tai Sakuma <tai.sakuma@gmail.com>
2020-04-04 14:20:10 -0400

    obtain publicPath from env var

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/~sakuma/cmsbook3/" : "/"
+  publicPath: process.env.VUE_APP_CMSBOOK_PUBLIC_PATH
 };
