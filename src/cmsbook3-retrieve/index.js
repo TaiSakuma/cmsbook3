594f248eb4a12d2af8096605e8f4745bbd527d0b
Tai Sakuma <tai.sakuma@gmail.com>
2020-04-05 16:36:36 -0400

    export functions for mocking

--- src/cmsbook3-retrieve/index.js
+++ src/cmsbook3-retrieve/index.js
@@ -8,3 +8,11 @@ export async function get_title() {
   }
   return response.data.title;
 }
+
+// export functions for ease of mocking
+// https://medium.com/@qjli/how-to-mock-specific-module-function-in-jest-715e39a391f4
+const exportFunctions = {
+  get_title,
+};
+
+export default exportFunctions;
