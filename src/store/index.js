f67bdd9272644b61297882c7090f55c215a75b83
Tai Sakuma <tai.sakuma@gmail.com>
2020-04-05 14:56:22 -0400

    clean code

--- src/store/index.js
+++ src/store/index.js
@@ -9,31 +9,32 @@ const state = {
   title: "cmsbook",
 };
 
-export const mutations = {
+const mutations = {
   set_title(state, title) {
     state.title = title;
+  },
+};
+
+const actions = {
+  async loadTitle({ commit }) {
+    const configUrl = process.env.VUE_APP_CMSBOOK_URL + "/.cmsbook3/title.json";
+    try {
+      const response = await axios.get(configUrl);
+      if (response.data.title == undefined) {
+        throw "title undefined";
+      }
+      const title = response.data.title;
+      commit("set_title", title);
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
 
 export const storeConfig = {
   state,
   mutations,
-  actions: {
-    async loadTitle({ commit }) {
-      const configUrl =
-        process.env.VUE_APP_CMSBOOK_URL + "/.cmsbook3/title.json";
-      try {
-        const response = await axios.get(configUrl);
-        if (response.data.title == undefined) {
-          throw "title undefined";
-        }
-        const title = response.data.title;
-        commit("set_title", title);
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  },
+  actions,
 };
 
 export default new Vuex.Store(storeConfig);
