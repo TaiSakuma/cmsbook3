98cfbfa406edaee1ac1781d3343050e3563eb2d7
Tai Sakuma <tai.sakuma@gmail.com>
2020-04-05 13:38:15 -0400

    update tests for store

--- tests/unit/store.spec.js
+++ tests/unit/store.spec.js
@@ -2,21 +2,50 @@ import { createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 import { cloneDeep } from "lodash";
 
+import moxios from "moxios";
+
 import { storeConfig } from "@/store/index.js";
 
 describe("store", () => {
+  const ENV_ORG = process.env;
   let localVue;
   let store;
 
   beforeEach(() => {
+    process.env.VUE_APP_CMSBOOK_URL = "http://localhost/cmsbook";
+    moxios.install();
     localVue = createLocalVue();
     localVue.use(Vuex);
     store = new Vuex.Store(cloneDeep(storeConfig));
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+    process.env = ENV_ORG;
   });
 
   it("commit set_title", () => {
     expect(store.state.title).toBe("cmsbook");
     store.commit("set_title", "new title");
     expect(store.state.title).toBe("new title");
+  });
+
+  it("dispatch loadTitle", async (done) => {
+    expect(store.state.title).toBe("cmsbook");
+    store.dispatch("loadTitle");
+    moxios.wait(async () => {
+      let request = moxios.requests.mostRecent();
+      expect(request.config.url).toBe(
+        "http://localhost/cmsbook/.cmsbook3/title.json"
+      );
+      await request.respondWith({
+        status: 200,
+        response: {
+          title: "new title",
+        },
+      });
+      expect(store.state.title).toBe("new title");
+      done();
+    });
   });
 });
