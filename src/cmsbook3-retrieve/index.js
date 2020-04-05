1c8f62f852627d94555e9f81f875af8383c1e810
Tai Sakuma <tai.sakuma@gmail.com>
2020-04-05 17:30:28 -0400

    extract retrieveDataFromPath() from getTitle()

--- src/cmsbook3-retrieve/index.js
+++ src/cmsbook3-retrieve/index.js
@@ -1,17 +1,24 @@
 import axios from "axios";
 
+export async function retrieveDataFromPath(path) {
+  const url = process.env.VUE_APP_CMSBOOK_URL + path;
+  const response = await axios.get(url);
+  return response.data;
+}
+
 export async function getTitle() {
-  const configUrl = process.env.VUE_APP_CMSBOOK_URL + "/.cmsbook3/title.json";
-  const response = await axios.get(configUrl);
-  if (response.data.title == undefined) {
+  const path = "/.cmsbook3/title.json";
+  const data = await retrieveDataFromPath(path);
+  if (data.title == undefined) {
     throw "title not found";
   }
-  return response.data.title;
+  return data.title;
 }
 
 // export functions for ease of mocking
 // https://medium.com/@qjli/how-to-mock-specific-module-function-in-jest-715e39a391f4
 const exportFunctions = {
+  retrieveDataFromPath,
   getTitle,
 };
 
