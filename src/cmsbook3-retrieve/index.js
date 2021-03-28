ee24bc9510878d35286761b722ece77e821fd480
Tai Sakuma <tai.sakuma@gmail.com>
2021-03-28 16:44:52 -0400

    add getSectionsInChapter()

--- src/cmsbook3-retrieve/index.js
+++ src/cmsbook3-retrieve/index.js
@@ -20,13 +20,24 @@ export async function getChapters() {
   return data.chapters;
 }
 
+export async function getSectionsInChapter(chapterPath) {
+  const path = `${chapterPath}/.cmsbook3/sections.json`;
+  const data = await lib.retrieveFrom(path);
+
+  if (data.sections == undefined) {
+    throw "sections not found";
+  }
+  return data.sections;
+}
+
 // export functions for ease of mocking
 // https://medium.com/@qjli/how-to-mock-specific-module-function-in-jest-715e39a391f4
 // https://luetkemj.github.io/170421/mocking-modules-in-jest
 export const lib = {
   retrieveFrom,
   getTitle,
-  getChapters
+  getChapters,
+  getSectionsInChapter
 };
 
 export default lib;
