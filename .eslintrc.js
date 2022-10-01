4fc4300d6268900237cc8a13ba65c07ca0375e96
Tai Sakuma <tai.sakuma@gmail.com>
2022-10-01 19:47:15 -0400

    upgrade webpack from 4 to 5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,29 @@
 module.exports = {
   root: true,
-
   env: {
-    node: true
+    node: true,
   },
-
   extends: ["plugin:vue/essential", "@vue/prettier"],
-
   rules: {
     "no-console": "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "vue/no-unused-components": "off",
+    "vue/multi-word-component-names": "off",
     "no-unused-labels": "off",
-    "no-unused-vars": "off"
+    "no-unused-vars": "off",
   },
-
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "@babel/eslint-parser",
   },
-
   overrides: [
     {
       files: [
         "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 };
