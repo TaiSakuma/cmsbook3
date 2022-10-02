3deddf4577c587754b16d564af9f12180b9bd589
Tai Sakuma <tai.sakuma@gmail.com>
2022-10-01 22:36:05 -0400

    rewrite Page with script setup

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -7,8 +7,8 @@
   </div>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, ref, watch, onUpdated } from "vue";
+<script setup lang="ts">
+import { computed, ref, watch, onUpdated } from "vue";
 import { useRoute } from "vue-router/composables";
 import { marked } from "marked";
 import $ from "jquery";
@@ -23,116 +23,103 @@ import "@/prism.css";
 
 import { retrieveFrom } from "@/cmsbook3-retrieve";
 
-export default defineComponent({
-  name: "page",
-  setup() {
-    const route = useRoute();
-    const cmsbook_url = ref(import.meta.env.VITE_CMSBOOK_URL);
-    const md = ref("");
+const route = useRoute();
+const cmsbook_url = ref(import.meta.env.VITE_CMSBOOK_URL);
+const md = ref("");
 
-    const path = computed(
-      () =>
-        `/${route.params.chapter}/${route.params.section}/${route.params.page}`
-    );
+const path = computed(
+  () => `/${route.params.chapter}/${route.params.section}/${route.params.page}`
+);
 
-    const content = computed(() => {
-      let htmlString: string;
-      try {
-        htmlString = marked.parse(md.value);
-      } catch (error) {
-        return "cannot parsed as markdown";
-      }
+const content = computed(() => {
+  let htmlString: string;
+  try {
+    htmlString = marked.parse(md.value);
+  } catch (error) {
+    return "cannot parsed as markdown";
+  }
 
-      try {
-        return editHtml(htmlString);
-      } catch (error) {
-        console.error(error);
-        return htmlString;
-      }
-    });
+  try {
+    return editHtml(htmlString);
+  } catch (error) {
+    console.error(error);
+    return htmlString;
+  }
+});
 
-    watch(
-      path,
-      async () => {
-        await getMarkDownFromPath();
-      },
-      { immediate: true }
-    );
-
-    async function getMarkDownFromPath() {
-      if (!path.value) {
-        md.value = "";
-        return;
-      }
-      try {
-        md.value = await retrieveFrom(path.value);
-      } catch (error) {
-        md.value = "Error: cannot get: " + path.value;
-      }
-    }
-
-    function editHtml(htmlString: string) {
-      const pathToCurrentDir = cmsbook_url.value + route.path.match(/.*\//);
-      const ret = editHtmlWithJQuery(htmlString, pathToCurrentDir);
-      return ret;
-    }
-
-    function editHtmlWithJQuery(htmlString: string, pathToCurrentDir: string) {
-      let tree = $(`<div>${htmlString}</div>`);
-      tree.find("a:not([href^='#'])").attr("target", "_blank");
-
-      tree
-        .find(
-          "a:not([href^='http:'],[href^='https:'],[href^='file:'],[href^='/'],[href^='#'])"
-        )
-        .each(function () {
-          this.setAttribute(
-            "href",
-            this.getAttribute("href").replace(/^/, pathToCurrentDir)
-          );
-        });
-
-      tree
-        .find("img:not([src^='http:'],[src^='https:'],[src^='/'])")
-        .each(function () {
-          this.setAttribute(
-            "src",
-            this.getAttribute("src").replace(/^/, pathToCurrentDir)
-          );
-        });
-
-      return tree.html();
-    }
-
-    const breadcrumbsItems = computed(() => {
-      let ret = [{ text: route.params.chapter }];
-      if (route.params.section) {
-        ret.push({ text: route.params.section });
-      }
-      if (route.params.page) {
-        route.params.page.split("/").forEach((e) => {
-          ret.push({ text: e });
-        });
-      }
-      return ret;
-    });
-
-    onUpdated(() => {
-      Prism.highlightAll();
-      try {
-        MathJax.Hub.Typeset();
-      } catch (error) {
-        console.log(error);
-      }
-    });
-
-    return {
-      cmsbook_url,
-      md,
-      content,
-      breadcrumbsItems,
-    };
+watch(
+  path,
+  async () => {
+    await getMarkDownFromPath();
   },
+  { immediate: true }
+);
+
+async function getMarkDownFromPath() {
+  if (!path.value) {
+    md.value = "";
+    return;
+  }
+  try {
+    md.value = await retrieveFrom(path.value);
+  } catch (error) {
+    md.value = "Error: cannot get: " + path.value;
+  }
+}
+
+function editHtml(htmlString: string) {
+  const pathToCurrentDir = cmsbook_url.value + route.path.match(/.*\//);
+  const ret = editHtmlWithJQuery(htmlString, pathToCurrentDir);
+  return ret;
+}
+
+function editHtmlWithJQuery(htmlString: string, pathToCurrentDir: string) {
+  let tree = $(`<div>${htmlString}</div>`);
+  tree.find("a:not([href^='#'])").attr("target", "_blank");
+
+  tree
+    .find(
+      "a:not([href^='http:'],[href^='https:'],[href^='file:'],[href^='/'],[href^='#'])"
+    )
+    .each(function () {
+      this.setAttribute(
+        "href",
+        this.getAttribute("href").replace(/^/, pathToCurrentDir)
+      );
+    });
+
+  tree
+    .find("img:not([src^='http:'],[src^='https:'],[src^='/'])")
+    .each(function () {
+      this.setAttribute(
+        "src",
+        this.getAttribute("src").replace(/^/, pathToCurrentDir)
+      );
+    });
+
+  return tree.html();
+}
+
+const breadcrumbsItems = computed(() => {
+  let ret = [{ text: route.params.chapter }];
+  if (route.params.section) {
+    ret.push({ text: route.params.section });
+  }
+  if (route.params.page) {
+    route.params.page.split("/").forEach((e) => {
+      ret.push({ text: e });
+    });
+  }
+  return ret;
+});
+
+onUpdated(() => {
+  Prism.highlightAll();
+  try {
+    MathJax.Hub.Typeset();
+  } catch (error) {
+    console.log(error);
+  }
 });
 </script>
 
