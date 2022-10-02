1ef13f1ceae08b9e94d89b4ec416ec859c1f32da
Tai Sakuma <tai.sakuma@gmail.com>
2022-10-01 23:13:11 -0400

    add type

--- src/cmsbook3-retrieve/index.ts
+++ src/cmsbook3-retrieve/index.ts
@@ -2,9 +2,32 @@ import { retrieveFrom } from "./retrieve";
 
 export { retrieveFrom };
 
+interface Title {
+  title?: string;
+}
+
+interface Path {
+  name: string;
+  path: string;
+}
+
+interface Paths {
+  chapters?: Path[];
+}
+
+interface Sections {
+  sections?: (
+    | Path
+    | {
+        name: string;
+        subcontents?: (Path | { name: string; subcontents: Path[] })[];
+      }
+  )[];
+}
+
 export async function getTitle() {
   const path = "/.cmsbook3/title.json";
-  const data = await lib.retrieveFrom(path);
+  const data = await lib.retrieveFrom<Title>(path);
   if (data.title == undefined) {
     throw "title not found";
   }
@@ -13,16 +36,16 @@ export async function getTitle() {
 
 export async function getChapters() {
   const path = "/.cmsbook3/chapters.json";
-  const data = await lib.retrieveFrom(path);
+  const data = await lib.retrieveFrom<Paths>(path);
   if (data.chapters == undefined) {
     throw "chapters not found";
   }
   return data.chapters;
 }
 
-export async function getSectionsInChapter(chapterPath) {
+export async function getSectionsInChapter(chapterPath: string) {
   const path = `${chapterPath}/.cmsbook3/sections.json`;
-  const data = await lib.retrieveFrom(path);
+  const data = await lib.retrieveFrom<Sections>(path);
 
   if (data.sections == undefined) {
     throw "sections not found";
--- src/cmsbook3-retrieve/retrieve.ts
+++ src/cmsbook3-retrieve/retrieve.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export async function retrieveFrom(path) {
+export async function retrieveFrom<T>(path: string) {
   const url = import.meta.env.VITE_CMSBOOK_URL + path;
-  const response = await axios.get(url);
+  const response = await axios.get<T>(url);
   return response.data;
 }
