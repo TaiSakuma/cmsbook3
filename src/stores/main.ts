ad61419004df3fe862fed033a3a156b23ca220ab
Tai Sakuma <tai.sakuma@gmail.com>
2022-10-02 08:57:11 -0400

    add packageVersion to store

--- src/stores/main.ts
+++ src/stores/main.ts
@@ -9,16 +9,24 @@ import {
 } from "@/cmsbook3-retrieve";
 
 export const useStore = defineStore("main", () => {
+  const packageVersion = ref(import.meta.env.PACKAGE_VERSION || "vx.x.x");
+
+  //
   const title = ref("");
+
   watchEffect(async () => {
     title.value = await getTitle();
   });
 
+  //
   const chapters = ref<Path[]>([]);
+
   watchEffect(async () => {
     chapters.value = await getChapters();
   });
+
   return {
+    packageVersion,
     title,
     chapters,
   };
