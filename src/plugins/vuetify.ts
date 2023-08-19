5c0ebb2b672aa3de20975f66bc1e92a099b4d975
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-19 08:56:15 -0400

    update vuetify config

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,11 +2,17 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { md3 } from "vuetify/blueprints";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 
+import * as labsComponents from "vuetify/labs/components";
+
+import "@/styles/variables.scss";
+
 const vuetify = createVuetify({
-  components,
+  components: { ...components, ...labsComponents },
   directives,
+  blueprint: md3,
   icons: {
     defaultSet: "mdi",
     aliases,
