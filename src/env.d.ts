b6b6f45f400d792316bf66163093a0138f9b2bdd
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-20 17:28:46 -0400

    remove env types
    moved to config

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,8 +3,6 @@
 interface ImportMetaEnv {
   readonly PACKAGE_VERSION: string;
   readonly VITE_PUBLIC_PATH: string
-  readonly VITE_CMSBOOK_URL: string
-  readonly VITE_CMSBOOK_INDEX_FILENAME: string
 }
 
 interface ImportMeta {
