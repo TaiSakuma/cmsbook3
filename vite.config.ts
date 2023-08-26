f16fc298e11be805fde4566c1f578d1ab8d65b67
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-26 10:17:59 -0400

    replace path-browserify in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import loadVersion from "vite-plugin-package-version";
-import path from "path-browserify";
 // import { defaultExclude } from "vitest/config";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
 
@@ -15,7 +15,7 @@ export default ({ mode }) => {
     base: process.env.VITE_PUBLIC_PATH,
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "src"),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
         path: "path-browserify",
       },
     },
