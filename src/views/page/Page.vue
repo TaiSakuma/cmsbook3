36cffe6d4c55dca85483b90edc105513b9f16571
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-26 16:19:17 -0400

    extract Breadcrumbs.vue from Page.vue

--- src/views/page/Page.vue
+++ src/views/page/Page.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page">
-    <v-breadcrumbs :items="breadcrumbsItems"></v-breadcrumbs>
+    <breadcrumbs></breadcrumbs>
     <div outlined max-width="980" class="ma-3 pa-3">
       <div class="markdown-body" v-html="content"></div>
     </div>
@@ -8,31 +8,13 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import { useRoute } from "vue-router";
-
 import { useContent } from "./content";
 import { usePrism } from "./prism";
 import { useMathJax } from "./mathjax";
 
-const route = useRoute();
+import Breadcrumbs from "./breadcrumbs/Breadcrumbs.vue";
 
 const { content } = useContent();
-
-const breadcrumbsItems = computed(() => {
-  let ret = [{ title: route.params.chapter }];
-  if (route.params.section) {
-    ret.push({ title: route.params.section });
-  }
-  if (Array.isArray(route.params.page)) {
-    route.params.page.forEach((e) => {
-      ret.push({ title: e });
-    });
-  } else if (route.params.page) {
-    ret.push({ title: route.params.page });
-  }
-  return ret;
-});
 
 usePrism();
 useMathJax();
---
+++ src/views/page/breadcrumbs/Breadcrumbs.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-breadcrumbs :items="items"></v-breadcrumbs>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const items = computed(() => {
+  let ret = [{ title: route.params.chapter }];
+  if (route.params.section) {
+    ret.push({ title: route.params.section });
+  }
+  if (Array.isArray(route.params.page)) {
+    route.params.page.forEach((e) => {
+      ret.push({ title: e });
+    });
+  } else if (route.params.page) {
+    ret.push({ title: route.params.page });
+  }
+  return ret;
+});
+</script>
