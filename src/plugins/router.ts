5a579acd5f0507a7fb816620b433f09942531c72
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-03 13:46:11 -0400

    rewrite an import path with @

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -14,7 +14,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: "/about",
     name: "about",
-    component: () => import("../views/About.vue"),
+    component: () => import("@/views/About.vue"),
   },
   {
     path: "/:chapter",
