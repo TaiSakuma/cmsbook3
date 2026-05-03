2617eca3812539c74f9cbd4ffc6d41319f34bee0
Tai Sakuma <tai.sakuma@gmail.com>
2026-05-03 12:11:51 -0400

    Commit an old untracked file

---
+++ src/views/page/MathJax.vue
@@ -0,0 +1,13 @@
+<template>
+  <div ref="mathjax">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useMathJax } from "./mathjax";
+
+const mathjax = ref<HTMLElement>();
+useMathJax(mathjax);
+</script>
